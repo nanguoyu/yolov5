9704aa18166c7568db3fd307639cfbe6c800881a
Dong Wang <dongwang@wangdongdong.wang>
2024-11-16 07:50:02 +0000

    remove save

--- train.py
+++ train.py
@@ -523,25 +523,25 @@ def train(hyp, opt, device, callbacks):
 
             # Save model
             if (not nosave) or (final_epoch and not evolve):  # if save
-                ckpt = {
-                    "epoch": epoch,
-                    "best_fitness": best_fitness,
-                    "model": deepcopy(de_parallel(model)).half(),
-                    "ema": deepcopy(ema.ema).half(),
-                    "updates": ema.updates,
-                    "optimizer": optimizer.state_dict(),
-                    "opt": vars(opt),
-                    "git": GIT_INFO,  # {remote, branch, commit} if a git repo
-                    "date": datetime.now().isoformat(),
-                }
+                # ckpt = {
+                #     "epoch": epoch,
+                #     "best_fitness": best_fitness,
+                #     "model": deepcopy(de_parallel(model)).half(),
+                #     "ema": deepcopy(ema.ema).half(),
+                #     "updates": ema.updates,
+                #     "optimizer": optimizer.state_dict(),
+                #     "opt": vars(opt),
+                #     "git": GIT_INFO,  # {remote, branch, commit} if a git repo
+                #     "date": datetime.now().isoformat(),
+                # }
 
-                # Save last, best and delete
-                torch.save(ckpt, last)
-                if best_fitness == fi:
-                    torch.save(ckpt, best)
-                if opt.save_period > 0 and epoch % opt.save_period == 0:
-                    torch.save(ckpt, w / f"epoch{epoch}.pt")
-                del ckpt
+                # # Save last, best and delete
+                # torch.save(ckpt, last)
+                # if best_fitness == fi:
+                #     torch.save(ckpt, best)
+                # if opt.save_period > 0 and epoch % opt.save_period == 0:
+                #     torch.save(ckpt, w / f"epoch{epoch}.pt")
+                # del ckpt
                 callbacks.run("on_model_save", last, epoch, final_epoch, best_fitness, fi)
 
         # EarlyStopping
